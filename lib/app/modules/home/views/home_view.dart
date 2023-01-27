09d704f653e617cfadfaba87d872b5a67389a386
riandwicahyo <riandwicahyo28@gmail.com>
2023-01-27 21:35:50 +0700

    modify controller and view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
+import 'package:check_delivery/app/data/models/province_model.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import '../../../data/models/city_model.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -12,11 +18,126 @@ class HomeView extends GetView<HomeController> {
         title: Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(24),
+        children: [
+          DropdownSearch<Province>(
+            popupProps: PopupProps.menu(
+              showSearchBox: true,
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text('${item.province}'),
+              ),
+            ),
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: 'Province',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            asyncItems: (text) async {
+              final response = await http.get(
+                Uri.parse('https://api.rajaongkir.com/starter/province'),
+                headers: {
+                  'key': 'bf32d82b4c142f1753cdb1508edabe5f',
+                },
+              );
+
+              if (response.statusCode != 200) {
+                return [];
+              }
+
+              Map<String, dynamic> data = jsonDecode(response.body);
+
+              return Province.fromJsonList(data['rajaongkir']['results']);
+            },
+            onChanged: (value) => controller.provId.value = value?.provinceId ?? '0',
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          DropdownSearch<City>(
+            popupProps: PopupProps.menu(
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text('${item.type} ${item.cityName}'),
+              ),
+            ),
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "City",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            asyncItems: (text) async {
+              var response = await http.get(
+                Uri.parse('https://api.rajaongkir.com/starter/city?province=${controller.provId}'),
+                headers: {'key': 'bf32d82b4c142f1753cdb1508edabe5f'},
+              );
+
+              Map<String, dynamic> data = jsonDecode(response.body);
+
+              var models = City.fromJsonList(data['rajaongkir']['results']);
+              return models;
+            },
+            onChanged:(value) => controller.citId.value = value?.cityId ?? '0',
+          ),
+
+          SizedBox(
+            height: 44,
+          ),
+          DropdownSearch<Province>(
+            popupProps: PopupProps.menu(
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text('${item.province}'),
+              ),
+            ),
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "Province",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            asyncItems: (text) async {
+              var response = await http.get(
+                Uri.parse('https://api.rajaongkir.com/starter/province'),
+                headers: {'key': 'bf32d82b4c142f1753cdb1508edabe5f'},
+              );
+
+              Map<String, dynamic> data = jsonDecode(response.body);
+
+              var models = Province.fromJsonList(data['rajaongkir']['results']);
+              return models;
+            },
+            onChanged:(value) => controller.nextProvId.value = value?.provinceId ?? '0',
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          DropdownSearch<City>(
+            popupProps: PopupProps.menu(
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text('${item.type} ${item.cityName}'),
+              ),
+            ),
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "City",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            asyncItems: (text) async {
+              var response = await http.get(
+                Uri.parse('https://api.rajaongkir.com/starter/city?province=${controller.nextProvId}'),
+                headers: {'key': 'bf32d82b4c142f1753cdb1508edabe5f'},
+              );
+
+              Map<String, dynamic> data = jsonDecode(response.body);
+
+              var models = City.fromJsonList(data['rajaongkir']['results']);
+              return models;
+            },
+            onChanged:(value) => controller.nextCitId.value = value?.cityId ?? '0',
+          ),
+        ],
       ),
     );
   }
